b8585345f6ed55893a17af15e85467fecd91a090
psantana5 <pausantanapi2@gmail.com>
2023-12-13 12:41:09 +0100

    Ej. Online Judge

---
+++ ejercicios/online-judge/p100.py
@@ -0,0 +1,9 @@
+int1 = int(input("1r integer: "))
+int2 = int(input("2do integer: "))
+
+while int1 < 10000 and int1 > 0 and int2 < 10000 and int2 > 0:
+    if int1 and int2 % 2 == 0:
+        
+
+else:
+    print("Numeros entre 0 y 10000 sólo")
