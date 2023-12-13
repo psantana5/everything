d801fcddd63018e4cb5c76cf1ca0836b909d60bb
psantana5 <pausantanapi2@gmail.com>
2023-12-13 12:42:02 +0100

    Online judge

--- ejercicios/online-judge/p100.py
+++ ejercicios/online-judge/p100.py
@@ -3,7 +3,7 @@ int2 = int(input("2do integer: "))
 
 while int1 < 10000 and int1 > 0 and int2 < 10000 and int2 > 0:
     if int1 and int2 % 2 == 0:
-        
+        pass
 
 else:
     print("Numeros entre 0 y 10000 sólo")
